9bff57df06f8d4c1a4bf7f22c030c488a0c5c38c
Kerem UÇAR <76447189+krmucr25@users.noreply.github.com>
2020-12-24 21:56:56 +0300

    Add files via upload

---
+++ first globalhubhomework.py
@@ -0,0 +1,11 @@
+name =(input('What is your name :'))
+surname =(input('what is your surname :'))
+age = int(input('How old are you :'))
+height = float(input('What is your height :'))
+weight = float(input('what is your weight :'))
+
+
+print(f'''  Name Type : {type(name)} \n surname Type : {type(surname)} \n Age Type : {type(age)} \n Height Type : {type(height)} \n weight Type : {type(weight)} ''')
+print(f'''  Name : {name} \n surname :{surname} \n Age : {age} \n Height : {height} \n weight : {weight}   ''')
+
+
